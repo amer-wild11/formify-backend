40e72508077643328ad63b44f9c07275bf64c1aa
amer-wild11 <amer33wild@gmail.com>
2025-01-25 18:33:03 +0300

    Test version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@prisma/nuxt"],
+  modules: ["@pinia/nuxt", "@prisma/nuxt", "@nuxt/icon"],
   css: ["~/assets/scss/main.scss"],
   ssr: true,
 });
