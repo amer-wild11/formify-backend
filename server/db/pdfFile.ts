9b22c3af9f8db3d5e92cdff486afa4b159faffbd
amer-wild11 <amer33wild@gmail.com>
2025-01-25 09:03:37 +0300

    Test version

--- server/db/pdfFile.ts
+++ server/db/pdfFile.ts
@@ -1,4 +1,6 @@
-import prisma from "~/lib/prisma";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const createPdfFile = async (data: any) => {
   const file = await prisma.pdfFile.create({ data });
